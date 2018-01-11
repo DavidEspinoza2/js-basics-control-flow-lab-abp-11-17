6840e7201a231bc0aadbfc699eafdb3cbc177300
David Espinoza <davidespinoza@att.net>
2018-01-11 09:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(lengthOfRideInFeet){
     if(lengthOfRideInFeet <= 400){
       return "This one is on me!";
     }
-  if(lengthOfRideInFeet > 2000){
+  if(lengthOfRideInFeet > 2000 && lengthOfRideInFeet < 2500){
     return "I will gladly take your thirty bucks.";
   }
   if(lengthOfRideInFeet > 2500){
