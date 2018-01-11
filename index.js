5ebab2d1ee04a2cf96a95ef39847433455736ab8
David Espinoza <davidespinoza@att.net>
2018-01-11 10:29:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(lengthOfRideInFeet){
-    if(lengthOfRideInFeet <= 400){
-      return "This one is on me!";
-    }
-  if(lengthOfRideInFeet > 2000 && lengthOfRideInFeet < 2500){
-    return "I will gladly take your thirty bucks.";
-  }
-  if(lengthOfRideInFeet > 2500){
-    return "No can do.";
-  }
-}
-
-function ternaryCheckCity(someString){
-  return someString === 'NYC' ? 'Ok, sounds good.' : 'No go.';
-}
 
 switch switchOnCharmFromTip(charm){
   case 'generous':
