fbbdbb4de7e0d2f963fb052182f2a35dd60f3cef
David Espinoza <davidespinoza@att.net>
2018-01-11 10:13:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,4 @@ function ternaryCheckCity(someString){
   return someString === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  case 'generous':
-    return "Thank you so much.";
-    break;
-  case 'not as generous':
-    return "Thank you.";
-    break;
-  case 'thanks for everything';
-    return 'Bye.';
-    break;
-}
+
