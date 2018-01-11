d84bf1662619744210dcde28edf81568f108d02f
David Espinoza <davidespinoza@att.net>
2018-01-11 10:28:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 // Write your code in this file!
 
-switch switchOnCharmFromTip(charm){
-  case 'generous':
-    return 'Thank you so much.';
-    break;
-  case 'not as generous':
-    return 'Thank you.';
-    break;
-  case 'thanks for everything':
-    return 'Bye.';
-    break;
-}
-
 function scuberGreetingForFeet(lengthOfRideInFeet){
     if(lengthOfRideInFeet <= 400){
       return "This one is on me!";
